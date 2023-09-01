be44c0746ac7dbdc7a71ebd342753972e1a8c4dd
Michael <michaellngriffin@gmail.com>
2023-09-01 14:02:58 -0400

    basic model/form made

--- forms.py
+++ forms.py
@@ -1,1 +1,14 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+
+class AddPetForm(FlaskForm):
+	#name, price corresponds to name of the to be created input
+	#string in field's parentheses correspond to the LABEL of the input
+    name = StringField("Pet Name")
+    species = StringField("Species")
+    photo_url = StringField("Image")
+    age = SelectField("Age",
+                choices = ['baby', 'young', 'adult', 'senior'])
+    notes = StringField("Notes")
--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 """Models for adopt app."""
 
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 
@@ -10,7 +9,50 @@ def connect_db(app):
 
     You should call this in your Flask app.
     """
-
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    """Pet."""
+    __tablename__ = "pets"
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    name = db.Column(
+        db.String(50),
+        nullable=False,
+        unique=True
+    )
+
+    species = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    photo_url = db.Column(
+        db.String,
+        nullable=False,
+        default=""
+    )
+
+    age = db.Column(
+        db.String(10),
+        db.CheckConstraint(age in ['baby', 'young', 'adult', 'senior']),
+        nullable=False
+    )
+
+    notes = db.Column(
+        db.String
+    )
+
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True
+    )
+
+
